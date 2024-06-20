2f11daf28e30aadb9f1cfd719eb05cd238fbaadd
pongpetr <suerdragon8995@gmail.com>
2024-06-20 22:20:50 +0700

    bp3-pang

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,12 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+for (let m = 0; m < scores.length; m++) {
+    if (m === 0) {
+        minScore = scores[m];
+    } else if ( minScore > scores[m]) {
+        minScore = scores[m]
+    }
+};
 
 console.log(minScore);
